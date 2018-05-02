784f81a315a3479a1ec6ea94bf689a3ed279a55a
zhuxiuxiu <zhuxiuxiu@bonc.com.cn>
2018-05-02 11:20:44 +0800

    as

--- Shha/src/main/java/com/jdk/base/TestRe.java
+++ Shha/src/main/java/com/jdk/base/TestRe.java
@@ -2,11 +2,12 @@ package com.jdk.base;
 
 public class TestRe {
 	public static void main(String[] args) {
-		char[] a = {'a','b','c'};
+		/*char[] a = {'a','b','c'};
 		char[] b = {'a','b','c'};
 		ReWriteEqual re = new ReWriteEqual(a);
 		ReWriteEqual re1 = new ReWriteEqual(b);
 		boolean c  = re.equals(re1);
-		System.out.println(c);
+		System.out.println(c);*/
+		Daimakuai dk = new Daimakuai();
 	}
 }
